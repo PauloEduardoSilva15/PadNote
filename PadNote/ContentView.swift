d47d416e0ffc61019d81aef8172ccbec04d48f2b
paulo <pauloeduardob75@gmail.com>
2026-07-17 16:09:36 -0300

    Initial Commit

---
+++ PadNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PadNote
+//
+//  Created by Paulo Eduardo Barbosa da Silva on 17/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PadNote/PadNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadNoteApp.swift
+//  PadNote
+//
+//  Created by Paulo Eduardo Barbosa da Silva on 17/07/26.
+//
+
+import SwiftUI
+
+@main
+struct PadNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
